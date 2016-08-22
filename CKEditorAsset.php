6afa874983ea88f0f890afd1d8e4c7a53593cc39
MrAnger <slava.sharapov@mail.ru>
2016-08-22 14:26:55 +0700

    Change require ckeditor package version

--- CKEditorAsset.php
+++ CKEditorAsset.php
@@ -9,7 +9,7 @@ use yii\web\View;
  * @author MrAnger
  */
 class CKEditorAsset extends AssetBundle {
-	public $sourcePath = "@vendor/ckeditor/ckeditor";
+	public $sourcePath = "@bower/ckeditor#full";
 
 	public $js = [
 		'ckeditor.js',
